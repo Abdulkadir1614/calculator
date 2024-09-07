5ba985dd5b769e52557965d2cddf4f5eb37ebe15
Abdulkadir1614 <abdukadir1614@gmail.com>
2024-09-07 16:17:58 +0800

    finished

--- challenge.js
+++ challenge.js
@@ -1,2 +1,24 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+function calculator (num1, num2, Parameter){
+    if (Parameter === "+"){
+         console.log(num1 + num2);
+    }
+    else if (Parameter === "-"){
+        console.log(num1 - num2);
+    }
+    else if (Parameter === "*"){
+        console.log(num1 * num2);
+    }
+    else if (Parameter === "/"){
+        console.log(num1 / num2);
+    }
+    else {
+        console.log("Invalid");
+    }
+}
+calculator(5, 3, "+"); 
+calculator(5, 3, "-"); 
+calculator(5, 3, "*"); 
+calculator(5, 3, "/");
